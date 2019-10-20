fd049c0694fd1b1b8482115d0fc2b7fe1680e25b
dpj <1115957667@qq.com>
2019-10-20 11:00:08 +0800

    update cpu version

--- captchaGenerator.py
+++ captchaGenerator.py
@@ -3,13 +3,13 @@ from captcha.image import ImageCaptcha  # pip install captcha
 from PIL import Image
 import random
 import time
-import captcha_setting
+import settings
 import os
 
 def random_captcha():
     captcha_text = []
-    for i in range(captcha_setting.MAX_CAPTCHA):
-        c = random.choice(captcha_setting.ALL_CHAR_SET)
+    for i in range(settings.MAX_CAPTCHA):
+        c = random.choice(settings.ALL_CHAR_SET)
         captcha_text.append(c)
     return ''.join(captcha_text)
 
@@ -22,7 +22,7 @@ def gen_captcha_text_and_image():
 
 if __name__ == '__main__':
     count = 1000
-    path = captcha_setting.TRAIN_DATASET_PATH    #通过改变此处目录，以生成 训练、测试和预测用的验证码集
+    path = settings.TRAIN_DATASET_PATH    #通过改变此处目录，以生成 训练、测试和预测用的验证码集
     if not os.path.exists(path):
         os.makedirs(path)
         
